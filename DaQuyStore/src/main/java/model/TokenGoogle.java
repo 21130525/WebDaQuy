c5391c088210e7f4bcf8ba25cfa7a4e764aa2486
Lê Thanh Tâm <tam1442k3@gmail.com>
2024-04-11 13:56:08 +0700

    comment secret google

--- DaQuyStore/src/main/java/model/TokenGoogle.java
+++ DaQuyStore/src/main/java/model/TokenGoogle.java
@@ -4,7 +4,7 @@ public class TokenGoogle {
 
     public static String GOOGLE_CLIENT_ID = "154679706679-29nmsp8c4r1phddtk3d4gcr1u5b5hic7.apps.googleusercontent.com";
 
-    public static String GOOGLE_CLIENT_SECRET = "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";
+//    public static String GOOGLE_CLIENT_SECRET = "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";
 
     public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/DaQuyStore_war/login";
 
